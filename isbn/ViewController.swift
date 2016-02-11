5df0c2235166bb685a8b23828667fc39cd0f7350
sergio albarran <sergio@myvoga.com>
2016-02-11 15:52:33 -0600

    Added cover image

--- isbn/ViewController.swift
+++ isbn/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var num: UITextField!
+    @IBOutlet weak var cover: UIImageView!
     @IBOutlet weak var result: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,21 +38,49 @@ class ViewController: UIViewController,UITextFieldDelegate {
                 let sesion = NSURLSession.sharedSession()
                 let bloque = { (datos: NSData?,resp: NSURLResponse?, error:  NSError? )-> Void in  dispatch_sync(dispatch_get_main_queue()) {
                     //let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
-                    do{
-                        //let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-                        //let d1 = json[0] as! NSDictionary
-                        var data = NSData(contentsOfURL: url!)
-                        if let json: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
-                            if let items = json["ISBN:978-84-376-0494-7"] as? NSArray {
-                                for item in items {
-                                    // construct your model objects here
+                    do {
+                        /*
+                            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                            let d1 = json[0] as! NSDictionary
+                            guard let item = d1["title"] as? [String: AnyObject],
+                                let book = item["ISBN:978-84-376-0494-7"] as? [String: AnyObject],
+                                let age = book["title"] as? String else {
+                                    return;
+                            }
+*/
+                        let jsonObject = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
+                        
+                        if let bookDictionary: AnyObject = jsonObject!["ISBN:\(self.num.text!)"] {
+                            let title : String = bookDictionary["title"] as! String
+                           self.result.text = "Titulo: \(title)\n"
+                            var names = [String]()
+                            if let authors = bookDictionary["authors"] as? NSArray {
+                                for author in authors {
+                                    if let author = author as? NSDictionary,
+                                        let name = author["name"] as? String {
+                                            names.append(name)
+                                    }
                                 }
                             }
-                        }
+                            let strinIng = names.joinWithSeparator(", ")
+                            self.result.text =                        self.result.text + "Autores: \n"  + strinIng
 
-                    }catch _ {
-                        
-                    }
+                            // Retrieve cover url
+                            var coverThumbURL: String = ""
+                            if let thumbs = bookDictionary["cover"] as? NSDictionary {
+                                let thumbnail = thumbs.valueForKey("medium") as? String
+                                coverThumbURL = thumbnail!
+                                let urlI = NSURL(string:coverThumbURL)
+                                
+                                self.result.text = self.result.text + "\nImagen: " + coverThumbURL
+
+                                let dataI = NSData(contentsOfURL: urlI!)
+                                    self.cover.image = UIImage(data: dataI!)
+                            }else{
+                                self.result.text = self.result.text + "\nImagen no disponible "
+                            }
+                        }
+                    }catch _ {}
                     
                     }
                 }
