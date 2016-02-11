cbca3c4dc1f47e47c01a0b99404f407894e6523b
sergio albarran <sergio@myvoga.com>
2016-02-11 15:55:13 -0600

    Ejemplo pre cargado de isbn

--- isbn/ViewController.swift
+++ isbn/ViewController.swift
@@ -110,9 +110,51 @@ class ViewController: UIViewController,UITextFieldDelegate {
                 let url = NSURL(string: urls)
                 let sesion = NSURLSession.sharedSession()
                 let bloque = { (datos: NSData?,resp: NSURLResponse?, error:  NSError? )-> Void in  dispatch_sync(dispatch_get_main_queue()) {
-                    let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
-                    print(texto!)
-                    self.result.text = texto! as String
+                    //let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
+                    do {
+                        /*
+                        let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                        let d1 = json[0] as! NSDictionary
+                        guard let item = d1["title"] as? [String: AnyObject],
+                        let book = item["ISBN:978-84-376-0494-7"] as? [String: AnyObject],
+                        let age = book["title"] as? String else {
+                        return;
+                        }
+                        */
+                        let jsonObject = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
+                        
+                        if let bookDictionary: AnyObject = jsonObject!["ISBN:\(self.num.text!)"] {
+                            let title : String = bookDictionary["title"] as! String
+                            self.result.text = "Titulo: \(title)\n"
+                            var names = [String]()
+                            if let authors = bookDictionary["authors"] as? NSArray {
+                                for author in authors {
+                                    if let author = author as? NSDictionary,
+                                        let name = author["name"] as? String {
+                                            names.append(name)
+                                    }
+                                }
+                            }
+                            let strinIng = names.joinWithSeparator(", ")
+                            self.result.text =                        self.result.text + "Autores: \n"  + strinIng
+                            
+                            // Retrieve cover url
+                            var coverThumbURL: String = ""
+                            if let thumbs = bookDictionary["cover"] as? NSDictionary {
+                                let thumbnail = thumbs.valueForKey("medium") as? String
+                                coverThumbURL = thumbnail!
+                                let urlI = NSURL(string:coverThumbURL)
+                                
+                                self.result.text = self.result.text + "\nImagen: " + coverThumbURL
+                                
+                                let dataI = NSData(contentsOfURL: urlI!)
+                                self.cover.image = UIImage(data: dataI!)
+                            }else{
+                                self.result.text = self.result.text + "\nImagen no disponible "
+                            }
+                        }
+                    }catch _ {}
+                    
                     }
                 }
                 let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
@@ -128,7 +170,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
             
             self.presentViewController(alertController, animated: true, completion: nil)
         }
-        return true
+        return true;
     }
     
     override func didReceiveMemoryWarning() {
